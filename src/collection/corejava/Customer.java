a9c6acca71f0212b82de98793451431fea75ea4f
Amar442 <amarkantkumar2512@gmail.com>
2025-01-19 11:45:31 +0530

    customer class is added

---
+++ src/collection/corejava/Customer.java
@@ -0,0 +1,24 @@
+package collection.corejava;
+
+public class Customer {
+
+	private String customerName;
+	private String[] customerAddress;
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public String[] getCustomerAddress() {
+		return customerAddress;
+	}
+
+	public void setCustomerAddress(String[] customerAddress) {
+		this.customerAddress = customerAddress;
+	}
+
+}
